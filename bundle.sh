72bc5d7b15290110dbd3fa993b4aa0562017c09b
Daniel K. O. (dkosmari) <none@none>
2024-03-23 20:17:40 -0300

    Changed from shell scripts to a Makefile, added ego link to README.
    --HG--
    branch : g-44

--- bundle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-exec gnome-extensions pack \
-     --force \
-     --podir=po \
-     --extra-source=AUTHORS \
-     --extra-source=COPYING \
-     --extra-source=README.md \
-     .
